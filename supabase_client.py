77369cf991ad302268d9f0b7c8b38a727159fce7
libranxp <130633300+libranxp@users.noreply.github.com>
2025-08-31 15:58:18 +0100

    supabase_client.py

---
+++ supabase_client.py
@@ -0,0 +1,8 @@
+# supabase_client.py
+import os
+from supabase import create_client, Client
+
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
